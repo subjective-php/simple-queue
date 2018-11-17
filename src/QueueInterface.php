600d009f112c0354cbd53cffa7348acb35d2f158
chadicus <chadwickgray@gmail.com>
2018-11-17 18:05:35 -0500

    use chadicus/coding-standard

--- src/QueueInterface.php
+++ src/QueueInterface.php
@@ -11,30 +11,30 @@ interface QueueInterface
      *
      * @return void
      *
-     * @throws Exception\DeleteException
+     * @throws Exception\DeleteException Thrown if the queue was unble to delete the message.
      */
-    public function deleteMessage(MessageInterface $messages);
+    public function deleteMessage(MessageInterface $message);
 
     /**
      * Delivers a message to the queue.
      *
      * @param MessageInterface $message The message to send.
-     * @param array            $options
+     * @param array            $options Options for sending the message.
      *
      * @return void
      *
-     * @throws Exception\SendException
+     * @throws Exception\SendException Thrown if the queue was unable to send the message.
      */
     public function sendMessage(MessageInterface $message, array $options);
 
     /**
      * Retrieves 0 or more messages from the specified.
      *
-     * @param array $options
+     * @param array $options Options for receiving the messages.
      *
      * @return MessageInterface[]
      *
-     * @throws Exception\ReceiveException
+     * @throws Exception\ReceiveException Thrown if the queue is unable to determine if messages can be received.
      */
     public function receiveMessages(array $options) : array;
 }
